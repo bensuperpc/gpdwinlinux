34ebdbebb7f860e103873f6cd0901ff3c02b548e
Bensuperpc <bensuperpc@gmail.com>
2021-06-17 05:36:12 +0200

    Improve scripts
    Improve scripts
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- grub.sh
+++ grub.sh
@@ -5,6 +5,8 @@ if [ "$UID" != "0" ]; then
 fi 
 
 mkdir -p /lib/firmware/edid
-echo "AP///////wAJ5QMAAwAAAAEdAQOACxF4LwAAoFdJmyYQSE8AAAABAQEBAQEBAQEBAQEBAQEBwhogUDAAEFAQEDIAbKwAAAAYAAAA/ABUVjA4MFdVTS1OTDAKAAAA/QA8PBAQBwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMM=" | base64 --decode > /lib/firmware/edid/gpdwinmax.bin
-echo 'GRUB_CMDLINE_LINUX="video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1"' >> /etc/default/grub
+echo "AP///////wAJ5QMAAwAAAAEdAQOACxF4LwAAoFdJmyYQSE8AAAABAQEBAQEBAQEBAQEBAQEBwhogUDAAEFAQEDIAbKwAAAAYAAAA/ABUVjA4MFdVTS1OTDAKAAAA/QA8PBAQBwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMM=" | base64 --decode > gpdwinmax.bin
+echo "12387d0061c5e778812f402452174f8174d4014216769dacac4e4daaaa6fcd6d gpdwinmax.bin" | sha256sum -c
+cp gpdwinmax.bin /lib/firmware/edid/gpdwinmax.bin
+echo 'GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX} video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1"' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
